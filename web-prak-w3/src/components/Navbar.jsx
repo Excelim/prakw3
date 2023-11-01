8c94519c547af4fafdcb6a1ac31ac943d71a20d6
Excelim <excelbimaa@gmail.com>
2023-11-02 00:49:32 +0700

    Commit

---
+++ web-prak-w3/src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import MyImg from 'D:/Semester 5/Web/prakw3/web-prak-w3/src/img/logo-ilab.png';
+function Navbar() {
+  return (
+    <nav className="d-flex justify-content-between align-items-center p-3">
+      <div>
+        <img src={MyImg} alt="" width={60} />
+      </div>
+      <ul className="nav navcstm">
+        <li className="nav-item">
+          <Link to="Home" className="nav-link">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/AboutUs" className="nav-link">About Us</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/Contact" className="nav-link">Contact</Link>
+        </li>
+      </ul>
+      <div className="d-flex">
+        <Button variant="primary" className="m-2">SIGN UP</Button>
+        <Button variant="light" className="m-2">  LOGIN  </Button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ web-prak-w3/src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Hero from 'D:/Semester 5/Web/prakw3/web-prak-w3/src/components/Hero.jsx';
+function Home() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
+
+export default Home;
